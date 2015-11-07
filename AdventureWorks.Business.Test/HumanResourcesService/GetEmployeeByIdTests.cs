4dcfb3188999e2fc87f56d9351c9d6720d7a5e8c
MattMcGowan <mattmcgowan1989@hotmail.com>
2015-11-07 15:29:00 +0000

    Unit testing project changes

---
+++ AdventureWorks.Business.Test/HumanResourcesService/GetEmployeeByIdTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventureWorks.Logic.Test.HumanResourcesService
+{
+    using NUnit.Framework;
+    using Moq;
+
+    [TestFixture]
+    public class GetEmployeeByIdTests
+    {
+        [Test]
+        public void GetEmployeeById_GivenExistantEmployeeId_ReturnsCorrectEmployee()
+        {
+
+        }
+
+        [Test]
+        public void GetEmployeeById_GivenNonExistantBusinessEntityId_ReturnNull()
+        {
+
+        }
+
+        [Test]
+        public void GetEmployeeById_GivenPersonNotEmployeeId_ReturnNull()
+        {
+
+        }
+    }
+}
